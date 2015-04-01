e73cb865971c0e7fe4cc2d0b5be60f31dcf8a16e
celston <celston@gmail.com>
2015-04-01 08:22:55 -0400

    missed files

---
+++ src/Stryker.PUPA/Surveys/ISurveyUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Gnosis.Entities.Attributes;
+
+namespace Stryker.PUPA.Surveys
+{
+    [EntityFieldsInterface]
+    public interface ISurveyUser
+    {
+        [EntityField]
+        Guid Survey { get; }
+        [EntityField]
+        string FirstName { get; }
+        [EntityField]
+        string LastName { get; }
+    }
+}
