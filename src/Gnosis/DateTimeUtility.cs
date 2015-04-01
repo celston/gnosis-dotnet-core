006760ca5077b844a6a708cb4c33e1804d48a82a
celston <celston@gmail.com>
2015-04-01 08:16:37 -0400

    First commit

---
+++ src/Gnosis/DateTimeUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnosis
+{
+    public static class DateTimeUtility
+    {
+        public static bool DateTimeEquals(DateTime? a, DateTime? b)
+        {
+            if (!a.HasValue)
+            {
+                if (b.HasValue)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (!b.HasValue)
+                {
+                    return false;
+                }
+            }
+
+            TimeSpan ts = a.Value - b.Value;
+
+            return ts.TotalSeconds < 1;
+        }
+    }
+}
---
+++ src/Stryker.PUPA/Surveys/SurveyUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Gnosis.Entities;
+
+namespace Stryker.PUPA.Surveys
+{
+    public class SurveyUser : Entity, ISurveyUser
+    {
+        public Guid Survey { get; protected set; }
+        public string FirstName { get; protected set; }
+        public string LastName { get; protected set; }
+    }
+}
